cf0b2e23bfe3f363a079b063540da9f3af87d67a
dugiabaok17 <97599798+dugiabaok17@users.noreply.github.com>
2022-09-25 16:37:59 +0700

    Add files via upload

---
+++ HoaDon.java
@@ -0,0 +1,100 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.poly.shop.entity;
+
+import com.poly.shop.hibernateConfig.HibernateConfig;
+import java.util.List;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Query;
+import javax.persistence.Table;
+import org.hibernate.Session;
+
+/**
+ *
+ * @author nguyenvv
+ */
+@Entity
+@Table(name = "HoaDon")
+public class HoaDon {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "maHoaDon")
+    private Integer maHoaDon;
+    
+    @Column(name = "tenKhachHang",length = 255)
+    private String tenKhachHang;
+    
+    @Column(name = "tongTien")
+    private Integer tongTien;
+    
+    
+    @Column(name = "diaChi", length = 255)
+    private String diaChi;
+
+    public HoaDon() {
+    }
+
+    public HoaDon(Integer maHoaDon, String tenKhachHang, Integer tongTien, String diaChi) {
+        this.maHoaDon = maHoaDon;
+        this.tenKhachHang = tenKhachHang;
+        this.tongTien = tongTien;
+        this.diaChi = diaChi;
+    }
+
+    public Integer getMaHoaDon() {
+        return maHoaDon;
+    }
+
+    public void setMaHoaDon(Integer maHoaDon) {
+        this.maHoaDon = maHoaDon;
+    }
+
+    public String getTenKhachHang() {
+        return tenKhachHang;
+    }
+
+    public void setTenKhachHang(String tenKhachHang) {
+        this.tenKhachHang = tenKhachHang;
+    }
+
+    public Integer getTongTien() {
+        return tongTien;
+    }
+
+    public void setTongTien(Integer tongTien) {
+        this.tongTien = tongTien;
+    }
+
+    public String getDiaChi() {
+        return diaChi;
+    }
+
+    public void setDiaChi(String diaChi) {
+        this.diaChi = diaChi;
+    }
+
+    @Override
+    public String toString() {
+        return "HoaDon{" + "maHoaDon=" + maHoaDon + ", tenKhachHang=" + tenKhachHang + ", tongTien=" + tongTien + ", diaChi=" + diaChi + '}';
+    }
+    
+    
+            
+    public static void main(String[] args) {
+        Session  session = HibernateConfig.getFACTORY().openSession();
+        Query query = session.createQuery("From ChiTietHoaDon");
+        List<ChiTietHoaDon> list = query.getResultList();
+        for (ChiTietHoaDon sanPham : list) {
+            System.out.println(sanPham);
+        }
+    }
+    
+    
+}
---
+++ SanPham.java
@@ -0,0 +1,96 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.poly.shop.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+/**
+ *
+ * @author nguyenvv
+ */
+@Entity
+@Table(name = "SanPham")
+public class SanPham {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Integer id;
+    
+    @Column(name = "tenSanPham", length = 255)
+    private String tenSanPham;
+    
+    @Column(name = "soLuong")
+    private Integer soLuong;
+    
+    @Column(name = "giaBan")
+    private Integer giaBan;
+    
+    @Column(name = "trangThai")
+    private Integer trangThai;
+
+    public SanPham(Integer id, String tenSanPham, Integer soLuong, Integer giaBan, Integer trangThai) {
+        this.id = id;
+        this.tenSanPham = tenSanPham;
+        this.soLuong = soLuong;
+        this.giaBan = giaBan;
+        this.trangThai = trangThai;
+    }
+
+    @Override
+    public String toString() {
+        return "SanPham{" + "id=" + id + ", tenSanPham=" + tenSanPham + ", soLuong=" + soLuong + ", giaBan=" + giaBan + ", trangThai=" + trangThai + '}';
+    }
+
+    public SanPham() {
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTenSanPham() {
+        return tenSanPham;
+    }
+
+    public void setTenSanPham(String tenSanPham) {
+        this.tenSanPham = tenSanPham;
+    }
+
+    public Integer getSoLuong() {
+        return soLuong;
+    }
+
+    public void setSoLuong(Integer soLuong) {
+        this.soLuong = soLuong;
+    }
+
+    public Integer getGiaBan() {
+        return giaBan;
+    }
+
+    public void setGiaBan(Integer giaBan) {
+        this.giaBan = giaBan;
+    }
+
+    public Integer getTrangThai() {
+        return trangThai;
+    }
+
+    public void setTrangThai(Integer trangThai) {
+        this.trangThai = trangThai;
+    }
+    
+    
+}
